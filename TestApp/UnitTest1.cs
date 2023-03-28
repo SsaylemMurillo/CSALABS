87d05126d27c62fdb8216f8adb41088aa5f671d3
Ssaylem Murillo <sandresmurillo@outlook.com>
2023-03-28 18:40:44 -0500

    update/add: It is confirmed working on Unit Testing

---
+++ TestApp/UnitTest1.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using DataAccessLayer;
+using Entity;
+using Presentation;
+using FluentAssertions;
+using BusinessLogicLayer;
+
+namespace TestApp
+{
+    public class Tests
+    {
+        Patient patient;
+        PatientService patientService;
+
+        [SetUp]
+        public void Setup()
+        {
+            patient = new Patient();
+            patientService = new PatientService(ConnectionStringExtractor.connectionString);
+        }
+
+
+        [Test]
+        public void TestSavePatient()
+        {
+            patient.Id = 123456789;
+            patient.IdType = "CC";
+            patient.FirstName = "pepi";
+            patient.SecondName = "pepito";
+            patient.LastName = "elpepe";
+            patient.SecondLastName = "elpepito";
+            patient.BornDate = new System.DateTime(1999, 12, 12);
+            patient.ExpeditionDate = new System.DateTime(2019, 12, 12);
+            patient.ExpeditionPlace = "Medellin";
+            patient.Phone = 123456789;
+            patient.Address = "Calle 123";
+            patient.Nacionality = "Colombia";
+
+
+            var response = patientService.SavePatient(patient);
+            var expected = "Paciente Correctamente Añadido";
+            var expected2 = "Ha ocurrido un error inesperado";
+
+            var possibleResponses = new List<string> { expected, expected2 };
+
+            Assert.Contains(expected, possibleResponses);
+            Assert.Pass();
+        }
+    }
+}
---
+++ TestPresentation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPresentation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPresentation")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc3d51dd-3efc-45b9-84a7-aba7f03504b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
